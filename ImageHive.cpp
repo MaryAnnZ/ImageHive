1a853c29aefb0366b9a3adff1944e417fa24b6a6
Rebeka <rebebaba2@gmail.com>
2017-05-03 12:18:39 +0200

    merge braches to update openCV 3

--- ImageHive.cpp
+++ ImageHive.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <opencv2\core.hpp>
 #include "opencv2\features2d\features2d.hpp"
-#include "opencv2\nonfree\features2d.hpp"
 #include <opencv\highgui.h>
 #include <opencv2\imgproc\imgproc.hpp>
 #include <Windows.h>
